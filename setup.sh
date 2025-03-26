ca59c90acc5708ce25292ee8372cb1dc2b7bb836
Reinert <reinert@dhc-gmbh.com>
2025-03-26 12:12:17 +0100

    setting correct permissions for .ssh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 grep -q "^alias ll=" ~/.bashrc || (echo "alias ll='ls --color=auto -alF'" >> ~/.bashrc && source ~/.bashrc)
 
 # ~/.ssh/
-mkdir -p ~/.ssh
-grep -q "lukas.reinert@dhc-gmbh.com$" ~/.ssh/authorized_keys 2>/dev/null || cat id_ed25519_reinert.pub >> ~/.ssh/authorized_keys
+mkdir -p ~/.ssh && chmod 700 ~/.ssh
+grep -q "lukas.reinert@dhc-gmbh.com$" ~/.ssh/authorized_keys 2>/dev/null || (cat id_ed25519_reinert.pub >> ~/.ssh/authorized_keys ; chmod 600 ~/.ssh/authorized_keys)
