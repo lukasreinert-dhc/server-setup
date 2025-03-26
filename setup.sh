e0fe673411cdcd7d1800730f2f489fbac3adea5c
Reinert <reinert@dhc-gmbh.com>
2025-03-26 14:31:45 +0100

    adding .vimrc

--- setup.sh
+++ setup.sh
@@ -4,3 +4,7 @@ grep -q "^alias ll=" ~/.bashrc || (echo "alias ll='ls --color=auto -alF'" >> ~/.
 # ~/.ssh/
 mkdir -p ~/.ssh && chmod 700 ~/.ssh
 grep -q "lukas.reinert@dhc-gmbh.com$" ~/.ssh/authorized_keys 2>/dev/null || (cat id_ed25519_reinert.pub >> ~/.ssh/authorized_keys ; chmod 600 ~/.ssh/authorized_keys)
+
+# ~/.vimrc
+grep -q "^set t_u7=" ~/.vimrc 2>/dev/null || echo "set t_u7=" >> ~/.vimrc # fix vim opening in replace mode
+grep -q "^set mouse-=a$" ~/.vimrc 2>/dev/null || echo "set mouse-=a" >> ~/.vimrc # fix entering visual mode when marking text
